a9ab899ac6d9512c763b2e609e949ba58dd77600
akpiinar <senaa.akpinarr@gmail.com>
2021-01-21 18:53:02 +0300

    sidebar düzenlendi ve kişi ekleme sayfası tasarlandı

--- Kullanici-Islemleri-Main/src/App.vue
+++ Kullanici-Islemleri-Main/src/App.vue
@@ -1,72 +1,65 @@
 <template>
 	<v-container
 		fluid
-		id="cont">
-
-		<v-row lg="12">
-			<v-app-bar
-				dark
-				flat
-			>
-<!--nav barda bulunan 3 çizgiye tıklandığında sidebarın gösterilip gizleneceği fonksiyon çalışıyor.-->
-				<v-app-bar-nav-icon @click="showOrHideMenu"></v-app-bar-nav-icon>
-			</v-app-bar>
-		</v-row>
-		<v-row lg="12">
-			<v-col lg="2">
-				<v-card
-					flat
+	>
+		<v-app>
+			<v-card>
+				<v-app-bar
+					dark
+					tile
 				>
-<!--					Side Barda bulunan sayfalara tıklandığında url adresine göre ilgili sayfaya yönlendirme yapılması için item.url i döndürüyoruz.-->
-					<v-navigation-drawer
-						permanent
-						:dark="true"
-						:style="displayNavBar"
-					>
-						<v-list
-							nav
+					<!--nav barda bulunan 3 çizgiye tıklandığında sidebarın gösterilip gizleneceği fonksiyon çalışıyor.-->
+					<v-app-bar-nav-icon @click="showOrHideMenu"></v-app-bar-nav-icon>
+				</v-app-bar>
+			</v-card>
+			<v-row
+			>
+				<v-col class="col-lg-2"
+				v-show="showMenu">
+			<v-navigation-drawer
+				:dark="true">
+
+				<v-list
+					flat
+					rounded
+				>
+					<v-list-item-group>
+						<v-list-item
+							v-for="(item,i) in items"
+							:key="i"
 						>
-							<v-list-item
-								v-for="item in items"
-								:key="item"
+							<!--alınan linki burada entegre edip sayfaya gitmesi için alıyoruz-->
+							<router-link
+								:to="{path:`${item.url}`}"
 							>
-<!--								alınan linki burada entegre edip sayfaya gitmesi için alıyoruz-->
-								<router-link
-									:to="{path:`${item.url}`}"
-								>
-									<v-list-item link>
-										<v-list-item-icon>
-											<v-icon>{{ item.icon }}</v-icon>
-										</v-list-item-icon>
-										<v-list-item-title>{{ item.title }}</v-list-item-title>
-									</v-list-item>
-								</router-link>
-							</v-list-item>
-						</v-list>
-					</v-navigation-drawer>
-				</v-card>
-			</v-col>
-			<v-col lg="10">
-<!--				main.js içinde verdiğimiz path isimlerine göre sayfalara yönlendirmeyi sağlaması için router-view tagını açıyoruz.-->
-				<router-view></router-view>
-			</v-col>
-		</v-row>
+								<v-list-item link>
+									<v-list-item-icon>
+										<v-icon v-text="item.icon"></v-icon>
+									</v-list-item-icon>
+									<v-list-item-title v-text="item.title"></v-list-item-title>
+								</v-list-item>
+							</router-link>
+						</v-list-item>
+					</v-list-item-group>
+				</v-list>
+			</v-navigation-drawer>
+				</v-col>
+				<v-col :class="showMenu ? 'lg-10' : 'lg-12'">
+					<router-view></router-view>
+				</v-col>
+			</v-row>
+			<!--				main.js içinde verdiğimiz path isimlerine göre sayfalara yönlendirmeyi sağlaması için router-view tagını açıyoruz.-->
 
 
+		</v-app>
 	</v-container>
-
-
-
 </template>
 
 <script>
-// import Dashboard from "@/components/Dashboard";
 export default {
 	name: 'App',
 
 	components: {
-		// Dashboard
-
 	},
 	data() {
 		return {
@@ -74,20 +67,21 @@ export default {
 			items: [
 				{title: 'Anasayfa', icon: 'mdi-view-dashboard', url: '/'},
 				{title: 'Kişi Ekle', icon: 'mdi-account-plus', url: '/kisiEkle'},
-				{title: 'kişi Listele', icon: 'mdi-account-group',url:'/kisiListesi'},
-				{title: 'Kişi Yüz Arama', icon: 'mdi-attachment',url:'kisiYuzSorgulama'},
-				{title: 'Uygulama Hakkinda', icon: 'mdi-information',url:'/Hakkinda'},
+				{title: 'kişi Listele', icon: 'mdi-account-group', url: '/kisiListesi'},
+				{title: 'Kişi Yüz Arama', icon: 'mdi-attachment', url: 'kisiYuzSorgulama'},
+				{title: 'Uygulama Hakkinda', icon: 'mdi-information', url: '/Hakkinda'},
 			],
-			displayNavBar: "display:none"
+			showMenu:true,
 		}
 	},
 	methods: {
 		//nav barda bulunan üç çizgili icona tıkladığımızda side barın açılıp açılmayacağını bu fonksiyon içerisinde kontrol ediyoruz.
 		showOrHideMenu() {
-			if (this.displayNavBar === "display:none") {
-				this.displayNavBar = "display:block"
+			if (this.showMenu) {
+				this.showMenu=false
+
 			} else {
-				this.displayNavBar = "display:none"
+				this.showMenu=true
 			}
 		}
 	}
