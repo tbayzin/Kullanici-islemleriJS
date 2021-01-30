3328d8bd1c5bb1dbd5797d35f7b214f246d3d1e8
akpiinar <senaa.akpinarr@gmail.com>
2021-01-30 22:45:34 +0300

    kişi listelemesi yapıldı

--- Kullanici-Islemleri-Main/src/components/kisiListesi.vue
+++ Kullanici-Islemleri-Main/src/components/kisiListesi.vue
@@ -1,16 +1,94 @@
-<template>
+<template id="kisiListesi">
+
 	<v-container
 		fluid
 	>
-		<p>Kişilerin Listeleneceği Sayfa</p>
+		<div id="app">
+			<v-app id="inspire">
+				<v-simple-table height="300px">
+					<template v-slot:default>
+						<thead>
+						<tr>
+							<th class="text-left">
+								Ad
+							</th>
+							<th class="text-left">
+								Soyad
+							</th>
+							<th class="text-left">
+								Telefon
+							</th>
+							<th class="text-left">
+								Doğum Tarihi
+							</th>
+							<th class="text-left">
+								E-Mail
+							</th>
 
+						</tr>
+						</thead>
+						<tbody>
+						<tr
+							v-for="(item,i) in personNames"
+							:key="item"
+						>
+							<td>{{personNames[i]}}</td>
+							<td>{{personSurnames[i]}}</td>
+							<td>{{phoneNumbers[i]}}</td>
+							<td>{{birthDates[i]}}</td>
+							<td>{{eMails[i]}}</td>
+						</tr>
+						</tbody>
+					</template>
+				</v-simple-table>
+			</v-app>
+		</div>
 	</v-container>
 </template>
 
 <script>
+
+
 export default {
-	name: "kisiListesi"
+	name: "kisiListesi",
+
+	data() {
+		return {
+
+			personNames: [],
+			personSurnames: [],
+			eMails: [],
+			phoneNumbers: [],
+			birthDates: [],
+
+
+		}
+	},
+	created() {
+		if (localStorage.getItem('personNames')) {
+			this.personNames = JSON.parse(localStorage.getItem('personNames'))
+
+		}
+		if (localStorage.getItem('personSurnames')) {
+			this.personSurnames = JSON.parse(localStorage.getItem('personSurnames'))
+
+		}
+		if (localStorage.getItem('phoneNumbers')) {
+			this.phoneNumbers = JSON.parse(localStorage.getItem('phoneNumbers'))
+
+		}
+		if (localStorage.getItem(	'eMails')) {
+			this.eMails = JSON.parse(localStorage.getItem('eMails'))
+		}
+		if (localStorage.getItem('birthDates')) {
+			this.birthDates = JSON.parse(localStorage.getItem('birthDates'))
+		}
+	}
+
+
 }
+
+// console.log(localStorage.getItem('personNames'))
 </script>
 
 <style scoped>
--- Kullanici-Islemleri-Main/src/main.js
+++ Kullanici-Islemleri-Main/src/main.js
@@ -1,18 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import VueRouter from 'vue-router'
-import Trend from "vuetrend"
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 import Dashboard from "@/components/Dashboard";
 import kisiEkle from "@/components/kisiEkle";
 import kisiListesi from "@/components/kisiListesi";
 import kisiYuzSorgulama from "@/components/kisiYuzSorgulama";
 import Hakkinda from "@/components/Hakkinda";
 
-Vue.config.productionTip = false
 
-Vue.use(Trend);
+
+Vue.config.productionTip = false;
+
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const router = new VueRouter({
 	//tanımladığımız rotaları spesifikleştirip ilgili url yönlendirmesi için eşleştirmeleri sağlıyoruz.
@@ -49,10 +51,8 @@ const router = new VueRouter({
 			}
 		}
 
-	]
+	],
 });
-
-
 
 
 new Vue({
@@ -60,4 +60,6 @@ new Vue({
 	render: h => h(App),
 	router,
 	components: {App},
+
+
 }).$mount('#app')
