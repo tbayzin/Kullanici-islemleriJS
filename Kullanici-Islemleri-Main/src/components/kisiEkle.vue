1f5bf022b4b360018b451d0933531e7244c8002b
akpiinar <senaa.akpinarr@gmail.com>
2021-01-31 18:36:09 +0300

    kisi ekleme sayfası tasarım düzenlemeleri

--- Kullanici-Islemleri-Main/src/components/kisiEkle.vue
+++ Kullanici-Islemleri-Main/src/components/kisiEkle.vue
@@ -6,19 +6,34 @@
 		<v-form>
 
 			<v-app id="inspire">
-				<v-row
-					no-gutters="no-gutters">
-					<v-col>
+				<v-row>
+					<v-col lg="4">
 						<v-text-field
-							label="Ad"
+							label="Ad*"
 							:rules="rules"
 							hide-details="auto"
 							:counter="100"
-							prepend-icon="mdi-account"
+							prepend-inner-icon="mdi-account"
 							v-model="personName"
+							outlined
+							shaped
+							clearable
 						></v-text-field>
 					</v-col>
-					<v-col>
+					<v-col lg="4">
+						<v-text-field
+							label="Soyad*"
+							:rules="rules"
+							hide-details="auto"
+							:counter="100"
+							prepend-inner-icon="mdi-account"
+							v-model="personSurname"
+							outlined
+							shaped
+							clearable
+						></v-text-field>
+					</v-col>
+					<v-col lg="4">
 						<v-menu
 							ref="menu"
 							v-model="menu"
@@ -32,10 +47,13 @@
 								<v-text-field
 									v-model="date"
 									label="Birthday date"
-									prepend-icon="mdi-calendar"
+									prepend-inner-icon="mdi-calendar"
 									readonly
 									v-bind="attrs"
 									v-on="on"
+									outlined
+									clearable
+									shaped
 
 								></v-text-field>
 							</template>
@@ -48,28 +66,8 @@
 							></v-date-picker>
 						</v-menu>
 					</v-col>
-				</v-row>
-				<v-row>
-					<v-col>
-						<v-text-field
-							label="Soyad"
-							:rules="rules"
-							hide-details="auto"
-							:counter="100"
-							prepend-icon="mdi-account"
-							v-model="personSurname"
-						></v-text-field>
-					</v-col>
-					<v-col>
-						<v-text-field
-							label="E-mail"
-							:rules="eMailRules"
-							hide-details="auto"
-							:counter="100"
-							prepend-icon="mdi-email"
-							v-model="eMail"
-						></v-text-field>
-					</v-col>
+
+
 				</v-row>
 				<v-row>
 					<v-col>
@@ -78,36 +76,68 @@
 							:rules="telephoneRules"
 							hide-details="auto"
 							:counter="15"
-							prepend-icon="mdi-phone"
+							prepend-inner-icon="mdi-phone"
 							v-model="phoneNumber"
+							outlined
+							shaped
+							clearable
 						></v-text-field>
 					</v-col>
 					<v-col>
 						<v-file-input
-							show-size
 							counter
-							multiple
 							label="File input"
+							multiple
+							prepend-inner-icon="mdi-paperclip"
+							outlined
+							shaped
+							:show-size="1000"
 						></v-file-input>
 					</v-col>
+					<v-col>
+						<v-text-field
+							label="E-mail"
+							:rules="eMailRules"
+							hide-details="auto"
+							:counter="100"
+							prepend-inner-icon="mdi-email"
+							v-model="eMail"
+							outlined
+							shaped
+							clearable
+						></v-text-field>
+					</v-col>
+
 				</v-row>
 				<v-row>
 					<v-col lg="2">
-					<v-btn
-						color="info"
-						@click="addPersons"
+						<v-btn
+							color="warning"
+							@click="addPersons"
 
-					>
-						Kaydet
-					</v-btn>
+						>
+							Kaydet
+						</v-btn>
 					</v-col>
-				<v-col lg="2">
-					<v-alert
-						type="success"
-						v-if="completed"
-					>Kaydedildi</v-alert>
-				</v-col>
-
+					<v-col></v-col>
+					<v-col lg="3">
+						<v-alert
+							type="success"
+							outlined
+							dense
+							v-if="cmp && complete"
+						>
+							KAYDEDİLDİ
+						</v-alert>
+						<v-alert
+							type="error"
+							outlined
+							dense
+							v-if="cmp && !complete"
+						>
+							LÜTFEN GEREKLİ ALANLARI DOLDURUNUZ
+						</v-alert>
+					</v-col>
 				</v-row>
 			</v-app>
 		</v-form>
@@ -122,16 +152,14 @@ export default {
 	data() {
 		return {
 			rules: [
-				value => !!value || 'Required',
+				value => !!value || 'Zorunlu alan',
 				value => (value && value.length >= 3) || 'Min 3 karakter'
 			],
 			telephoneRules: [
-				value => !!value || 'Required',
 				value => (value && value.length >= 11) || 'Telefon 11 karakterden az olamaz'
 			],
 			eMailRules: [
-				value => !!value || 'E-mail is required',
-				value => /.+@+/.test(value) || 'E-mail zorunlu'
+				value => /.+@+/.test(value)
 			],
 			date: null,
 			menu: false,
@@ -145,75 +173,75 @@ export default {
 			personSurname: '',
 			phoneNumber: '',
 			eMail: '',
-			completed: false
-
+			complete: false,
+			cmp: false
 
 		}
-
 	},
 	methods: {
 		save(date) {
 			this.$refs.menu.save(date)
 		},
 		addPersons() {
-			this.personNames.push(this.personName);
-			this.personSurnames.push(this.personSurname);
-			this.phoneNumbers.push(this.phoneNumber);
-			this.eMails.push(this.eMail);
-			this.birthDates.push(this.date);
-			this.personName = ''
-			this.personSurname = ''
-			this.phoneNumber = ''
-			this.eMail = ''
-			this.date = ''
-			this.completed = true
+			this.cmp = true
+			if (this.personName === '' || this.personSurname === '') {
+				console.log("kkldfk")
+				this.complete = false
+			} else {
+				this.personNames.push(this.personName);
+				this.personSurnames.push(this.personSurname);
+				this.phoneNumbers.push(this.phoneNumber);
+				this.eMails.push(this.eMail);
+				this.birthDates.push(this.date);
+				this.personName = ''
+				this.personSurname = ''
+				this.phoneNumber = ''
+				this.eMail = ''
+				this.date = ''
+				this.complete = true
 
-		},
-		watch: {
-			menu(val) {
-				val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
-			},
-			personNames: {
-				handler() {
-					localStorage.setItem('personNames', JSON.stringify(this.personNames))
-				},
-				deep: true
-			},
-			personSurnames: {
-				handler() {
-					localStorage.setItem('personSurnames', JSON.stringify(this.personSurnames))
-				},
-				deep: true
-
-			},
-			phoneNumbers: {
-				handler() {
-					localStorage.setItem('phoneNumbers', JSON.stringify(this.phoneNumbers))
-				}
-			},
-			eMails: {
-				handler() {
-					localStorage.setItem('eMails', JSON.stringify(this.eMails))
-				}
-			},
-			birthDates: {
-				handler() {
-					localStorage.setItem('birthDates', JSON.stringify(this.birthDates))
-				}
 			}
-
-
 		},
-
-
-	}
+	},
+	watch: {
+		menu(val) {
+			val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+		},
+		personNames: {
+			handler() {
+				localStorage.setItem('personNames', JSON.stringify(this.personNames))
+			},
+			deep: true
+		},
+		personSurnames: {
+			handler() {
+				localStorage.setItem('personSurnames', JSON.stringify(this.personSurnames))
+			},
+			deep: true
+		},
+		phoneNumbers: {
+			handler() {
+				localStorage.setItem('phoneNumbers', JSON.stringify(this.phoneNumbers))
+			}
+		},
+		eMails: {
+			handler() {
+				localStorage.setItem('eMails', JSON.stringify(this.eMails))
+			}
+		},
+		birthDates: {
+			handler() {
+				localStorage.setItem('birthDates', JSON.stringify(this.birthDates))
+			}
+		}
+	},
 }
 </script>
+
 
 <style scoped>
 #container {
 	margin-left: 200px;
 	padding: 50px;
 }
-
 </style>
